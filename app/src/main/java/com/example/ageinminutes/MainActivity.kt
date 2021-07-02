e694845d438d69181cd67d988ca6668d68508410
Tbonator <thabo2g5@yahoo.ca>
2021-07-03 01:26:02 +0200

    initial commit

---
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.ageinminutes
+
+import android.app.DatePickerDialog
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ageinminutes.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnDatePicker.setOnClickListener {
+                view -> clickDatePicker(view)
+            //Toast.makeText(this,"Button Works",Toast.LENGTH_LONG).show()
+        }
+
+
+    }
+    fun clickDatePicker(view: View) {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this,DatePickerDialog.OnDateSetListener {
+                view, selectedYear, selectedMonth, selectedDayOfMonth ->
+
+            val selectedDate = "$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
+            binding.tvSelectedDate.text = selectedDate
+
+            val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy",Locale.ENGLISH)
+
+            val theDate = simpleDateFormat.parse(selectedDate)
+
+            val currentDate = simpleDateFormat.parse(simpleDateFormat.format(System.currentTimeMillis()))
+            val selectedDateInMinutes = theDate!!.time / 60000
+            val currentDateInMinutes = currentDate!!.time / 60000
+
+            binding.tvSelectedDateInMinutes.text = (currentDateInMinutes - selectedDateInMinutes).toString()
+        }
+       ,year
+       ,month
+       ,day).show()
+
+    }
+
+
+}
